e6cc8fb3043d6aa4fad99724b660c7c0832e5ed6
Martiyn <etigomartin4o0o@gmail.com>
2021-04-26 10:55:34 +0300

    wrapped up with the output

---
+++ src/output.js
@@ -0,0 +1,11 @@
+class Output extends React.Component {
+    render() {
+      return (
+        <div className="outputScreen" id="display">
+          {this.props.currentValue}
+        </div>
+      );
+    }
+  }
+
+export default Output
